e786d1348e49568c3a8e039261ba24983ebfb006
trisselj <trisselj@oregonstate.edu>
2024-07-22 13:29:56 -0700

    q

--- multiply.py
+++ multiply.py
@@ -3,7 +3,7 @@
 # Date: 07/17/2024
 # Description: Multiplies two numbers together by using addition instead of the built in multiplication
 
-# Defining out function as multiply, returns the product of two integers using recursion
+# Defines function as multiply, returns the product of two integers using recursion
 def multiply(a, b):
     if b == 1: # Returns "a" if multiply by one
         return a
